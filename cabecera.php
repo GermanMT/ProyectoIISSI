f568008e6ab9f11d72e0af6ab190881404944c44
Alvarofb7 <alvarofb1998@gmail.com>
2019-05-10 19:14:09 +0200

    Modificaciones

---
+++ cabecera.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html lang="es">
+<head>
+  <meta charset="utf-8">
+  <link rel="stylesheet" type="text/css" href="CSS/styleIISSI.css" />
+  <title>Header</title>
+</head>
+<header>
+    <div class="header">
+        <div class="header_box">
+            <div class="logo">
+                <a href="IISSI.html">Academia 2G</a>
+            </div>
+        <div class="detail">
+            <div class="login" style="
+                width: 121.198">
+                <img src="images/logoAcademia.png">
+            </div>
+		</div>
+    </div>
+</header>
+</html>
