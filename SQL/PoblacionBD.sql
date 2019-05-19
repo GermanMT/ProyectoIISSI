eed5fb5769144299015dcf56c4fa9d4069ec6b8c
Josan <josanfcs98@hotmail.com>
2019-05-19 19:15:42 +0100

    Revert "Revert "Merge remote-tracking branch 'origin/Josan' into German""
    This reverts commit 10124fe39d008c65c97f48104716d71668e54ec5.

--- SQL/PoblacionBD.sql
+++ SQL/PoblacionBD.sql
@@ -5,13 +5,11 @@ COMMIT;
 END;
 /
 
---Inserciones de alumnos:
-BEGIN
-INSERTAR_USUARIO('21015236K','Antonio German','Márquez Trujillo','21', 'El Repilado', '619527721', '954841642', 'antonio-german-1@hotmail.com', 'Jose Enrique', 'GermanOctavo', '123456','Alumno');
-INSERTAR_USUARIO('67843287L','Jose Antonio','Macias Portillo','20', 'La Puebla de Cazalla', '619527721', '954841642', 'jose-antonio@hotmail.com', 'Gustavo', 'JosanFCS', '123456','Alumno');
-INSERTAR_USUARIO('75867742J','Alvaro','Frias Balbuena','22', 'Arahal', '619527721', '954841642', 'alvaro-231@hotmail.com', 'Pedro', 'AlavaroFB7', '123456','Alumno');
-INSERTAR_USUARIO('22222222K','Octavio','Márquez','21', 'El Repilado', '619527721', '954841642', 'antonio-german-1@hotmail.com', 'Jose Enrique', 'Profesor', 'profesor','Profesor');
+INSERTAR_USUARIO('21015236K','Antonio German','Márquez','21', 'El Repilado', '619527721', '954841642', 'antonio-german-1@hotmail.com', 'Jose Enrique', 'GermanOctavo', '19german15','Alumno');
+INSERTAR_USUARIO('67843287L','Jose Antonio','Macias Portillo','20', 'La Puebla de Cazalla', '619527721', '954841642', 'jose-antonio@hotmail.com', 'Gustavo', 'Josan', '123456','Alumno');
+INSERTAR_USUARIO('75867742J','Alavaro','Frias Balbuena','22', 'Arahal', '619527721', '954841642', 'alvaro-231@hotmail.com', 'Pedro', 'AlavaroFB7', '123456','Alumno');
 INSERTAR_USUARIO('11111111K','Admin','Admin','1', 'Arahal', '698765432', '123456789', 'academia@hotmail.com', 'Jose Enrique', 'Admin', 'administrador','Admin');
+INSERTAR_USUARIO('49385388H','Jose Antonio','Macias','20', 'La Puebla De Cazalla', '619525521', '954822642', 'josanfcs98@hotmail.com', 'Jose Enrique', 'JosanFCS', 'holita20','Profesor');
 COMMIT;
 END;
 /
@@ -32,11 +30,28 @@ COMMIT;
 END;
 /
 
---Inserciones de horarios de profesores:
+--Inserciones de academias:
 BEGIN
-INSERTAR_HORARIO_PROFESOR('12:00', '14:00','Lunes', SEC_Horario_Profesor.NEXTVAL, '11111111K',1);
-INSERTAR_HORARIO_PROFESOR('17:00', '19:00','Miercoles', SEC_Horario_Profesor.NEXTVAL, '11111111K',1);
-INSERTAR_HORARIO_PROFESOR('12:00', '14:00','Viernes', SEC_Horario_Profesor.NEXTVAL, '11111111K',1);
+INSERTAR_ACADEMIA('Arahal','5',SEC_Academias.NEXTVAL,'698765432');
+COMMIT;
+END;
+/
+--Inserciones de cursos:
+BEGIN
+INSERTAR_CURSO('20/05/2018','20/12/2018','10',SEC_Cursos.NEXTVAL,SEC_Academias.CURRVAL);
+INSERTAR_CURSO('20/03/2018','20/10/2018','12',SEC_Cursos.NEXTVAL,SEC_Academias.CURRVAL);
+INSERTAR_CURSO('20/02/2018','20/09/2018','8',SEC_Cursos.NEXTVAL,SEC_Academias.CURRVAL);
+INSERTAR_CURSO('20/01/2018','20/08/2018','11',SEC_Cursos.NEXTVAL,SEC_Academias.CURRVAL);
+COMMIT;
+END;
+/
+
+
+--Inserciones de Horario para profesor:
+BEGIN
+INSERTAR_HORARIO_PROFESOR('16:00', '18:00', 'Martes',SEC_Horario_Profesor.NEXTVAL,SEC_Cursos.CURRVAL,'67843287L');
+INSERTAR_HORARIO_PROFESOR('17:00', '19:00', 'Miercoles',SEC_Horario_Profesor.NEXTVAL,SEC_Cursos.CURRVAL,'21015236K');
+INSERTAR_HORARIO_PROFESOR('15:00', '17:00', 'Jueves',SEC_Horario_Profesor.NEXTVAL,SEC_Cursos.CURRVAL,'75867742J');
 COMMIT;
 END;
 /
--- gestionarCurso.php
+++ gestionarCurso.php
@@ -1,13 +1,13 @@
-﻿<?php
+<?php
   /*
      * #===========================================================#
      * #	Este fichero contiene las funciones de gestión     			 
-     * #	de horarios de profesores de la capa de acceso a datos 		
+     * #	de Cursos de la capa de acceso a datos 		
      * #==========================================================#
      */
-function consultarTodosHorariosProfesor($conexion) {
-	$consulta = "SELECT * FROM Cursos, Horario_Profesor "
-		. " WHERE (Horario_Profesor.Id_Curso = Cursos.Id_Curso)";
+function consultarTodosCursosProfesor($conexion) {
+	$consulta = "SELECT * FROM Cursos_Alumnos, Usuarios "
+		. " WHERE (Cursos_Alumnos.DNI_Usuario = Usuarios.DNI_Usuario)";
     return $conexion->query($consulta);
 }
     
