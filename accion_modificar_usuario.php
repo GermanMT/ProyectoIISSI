4eb8b5ee672a6368d64f8a1e110df2b808090745
Josan <josanfcs98@hotmail.com>
2019-05-29 19:36:08 +0100

    Vista Admin sol

--- accion_modificar_usuario.php
+++ accion_modificar_usuario.php
@@ -16,11 +16,11 @@
 			
 		if ($excepcion<>"") {
 			$_SESSION["excepcion"] = $excepcion;
-			$_SESSION["destino"] = "vistaAdmin.php";
+			$_SESSION["destino"] = "vistaAdminCurso.php?var2=" . $_GET["var2"];
 			Header("Location: excepcion.php");
 		}
 		else
-			Header("Location: vistaAdmin.php");
+			Header("Location: vistaAdminCurso.php?var2=" . $_GET["var2"]);
 	} 
-	else Header("Location: vistaAdmin.php"); // Se ha tratado de acceder directamente a este PHP
+	else Header("Location: vistaAdminCurso.php?var2=" . $_GET["var2"]); // Se ha tratado de acceder directamente a este PHP
 ?>
