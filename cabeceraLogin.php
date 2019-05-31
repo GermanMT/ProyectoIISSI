1a3546c28f6af7a19f9f21f62e730678ca92f01a
GermanTheWanker <44214733+GermanTheWanker@users.noreply.github.com>
2019-05-31 19:06:38 +0200

    s

--- cabeceraLogin.php
+++ cabeceraLogin.php
@@ -10,7 +10,7 @@
     <!--<div class="header">-->
       <div class="header_box" style="margin-top: 100px;">
             <div class="logo" style="margin-left: 100px;">
-                <a href="IISSI.html">Academia 2G</a>
+                <a href="IISSI.php">Academia 2G</a>
             </div>
         <div class="detail" style="margin-right: 100px;">
             <div class="login" style="
